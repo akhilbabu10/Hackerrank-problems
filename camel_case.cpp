87b872a73e67eda508d043bb3178eec8fd7fbf16
akhilbabu10 <44550438+akhilbabu10@users.noreply.github.com>
2020-10-30 20:36:14 +0530

    added camel_case.cpp

---
+++ camel_case.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the camelcase function below.
+int camelcase(string s) {
+    int l=s.length();
+    long int c=0;
+    for(int i=0;i<l;i++)
+    {
+        if(isupper(s[i]))
+            c++;
+    }
+    return c+1;
+    
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
